83a1d14e47f0e49c2c6c87ef1596f206c3357015
Tyler Lartonoix <tyler@lartonoix.com>
2023-08-11 12:33:01 -0400

    Add test for Projection class

---
+++ tests/Projection/ProjectionTest.php
@@ -0,0 +1,166 @@
+<?php
+	namespace Projection;
+
+	use DaybreakStudios\DoctrineQueryDocument\Projection\AllowResult;
+	use DaybreakStudios\DoctrineQueryDocument\Projection\Projection;
+	use PHPUnit\Framework\TestCase;
+
+	class ProjectionTest extends TestCase {
+		public function testQuery() {
+			$projection = Projection::fromFields([
+				'a.b.c' => true,
+			]);
+
+			$result = $projection->query('a.b.c');
+			$this->assertEquals(AllowResult::allow(true), $result, 'reports explicit allows');
+
+			$result = $projection->query('1.2.3');
+			$this->assertEquals(AllowResult::deny(), $result, 'reports implicit denies');
+
+			$projection = Projection::fromFields([
+				'a.b.c' => false,
+			]);
+
+			$result = $projection->query('a.b.c');
+			$this->assertEquals(AllowResult::deny(true), $result, 'reports explicit denies');
+
+			$result = $projection->query('1.2.3');
+			$this->assertEquals(AllowResult::allow(), $result, 'reports implicit allows');
+
+			$projection = Projection::fromFields([
+				'a.b' => true,
+			]);
+
+			$result = $projection->query('a.b.c');
+			$this->assertEquals(
+				AllowResult::allow(true),
+				$result,
+				'explicit allows on parent nodes are inherited by child nodes',
+			);
+
+			$projection = Projection::fromFields([
+				'a.b' => false,
+			]);
+
+			$result = $projection->query('a.b.c');
+			$this->assertEquals(
+				AllowResult::deny(true),
+				$result,
+				'explict denys on parent nodes are inherited by child nodes',
+			);
+		}
+
+		public function testIsAllowedByDefault() {
+			$projection = Projection::fromFields([
+				'a.b.c' => true,
+				'1.2.3' => true,
+			]);
+
+			$this->assertFalse($projection->isAllowedByDefault(), 'infers default deny from allow-list');
+
+			$projection = Projection::fromFields([
+				'a.b.c' => false,
+				'1.2.3' => false,
+			]);
+
+			$this->assertTrue($projection->isAllowedByDefault(), 'infers default allow from deny-list');
+
+			$projection = Projection::fromFields([
+				'a.b.c' => true,
+				'1.2.3' => false,
+			]);
+
+			$this->assertFalse($projection->isAllowedByDefault(), 'infers default from initial value in mixed list');
+		}
+
+		public function testIsDenied() {
+			$projection = Projection::fromFields([
+				'a.b.c' => false,
+			]);
+
+			$this->assertTrue($projection->isDenied('a.b.c'), 'matches explicit deny');
+
+			$projection = Projection::fromFields([
+				'a.b.c' => true,
+			]);
+
+			$this->assertTrue($projection->isDenied('1.2.3'), 'matches implicit deny');
+		}
+
+		public function testIsDeniedExplicitly() {
+			$projection = Projection::fromFields([
+				'a.b.c' => false,
+			]);
+
+			$this->assertTrue($projection->isDeniedExplicitly('a.b.c'), 'matches explicit deny');
+
+			$projection = Projection::fromFields([
+				'a.b.c' => true,
+			]);
+
+			$this->assertFalse($projection->isDeniedExplicitly('1.2.3'), 'does not match implicit deny');
+		}
+
+		public function testIsAllowed() {
+			$projection = Projection::fromFields([
+				'a.b.c' => true,
+			]);
+
+			$this->assertTrue($projection->isAllowed('a.b.c'), 'matches explicit allow');
+
+			$projection = Projection::fromFields([
+				'a.b.c' => false,
+			]);
+
+			$this->assertTrue($projection->isAllowed('1.2.3'), 'matches implicit allow');
+		}
+
+		public function testIsAllowedExplicitly() {
+			$projection = Projection::fromFields([
+				'a.b.c' => true,
+			]);
+
+			$this->assertTrue($projection->isAllowedExplicitly('a.b.c'), 'matches explicit allow');
+
+			$projection = Projection::fromFields([
+				'a.b.c' => false,
+			]);
+
+			$this->assertFalse($projection->isAllowedExplicitly('1.2.3'), 'does not match implicit allow');
+		}
+
+		public function testFilter() {
+			$projection = Projection::fromFields([
+				'a' => true,
+				'b' => true,
+				'd' => true,
+			]);
+
+			$this->assertEquals(
+				['a' => 1, 'b' => 2, 'd' => 4],
+				$projection->filter([
+					'a' => 1,
+					'b' => 2,
+					'c' => 3,
+					'd' => 4,
+				]),
+				'removes unmatched keys from allow-list',
+			);
+
+			$projection = Projection::fromFields([
+				'a' => false,
+				'c' => false,
+			]);
+
+			$this->assertEquals(
+				['b' => 2, 'd' => 4],
+				$projection->filter([
+					'a' => 1,
+					'b' => 2,
+					'c' => 3,
+					'd' => 4,
+				]),
+				'removes matched keys from deny-list',
+			);
+		}
+	}
